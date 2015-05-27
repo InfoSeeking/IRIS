d33322c0a5e42501503d3d1ebc714090fabd33da
Kevin Albertson <ka324@scarletmail.rutgers.edu>
2015-05-26 22:29:44 -0400

    Adds simple testing script

--- tests/test.php
+++ tests/test.php
@@ -1,9 +1,24 @@
 <?php
-//$API_ENDPOINT = "http://iris.comminfo.rutgers.edu";
+/*
+This file is used to test if your local IRIS setup is functioning.
+Run it via command line like so:
+php -f test.php -- <input_file>
+
+Change API_ENDPOINT to match yours if your setup differs.
+*/
 $API_ENDPOINT = "http://localhost/IRIS/index.php";
 //using this proxy, the IP address is from the web server, not the client's computer
 
-if(!isset($_POST['xmldata'])) die("Nothing passed");
+if(count($argv) > 1){
+  $data = file_get_contents($argv[1]);
+  if(!$data){
+    die("Could not read file " . $argv[1]);
+  }
+} else {
+  echo "Usage: php -f test.php -- <input_file>\n";
+  echo "Example: php -f test.php -- examples/sample_filter.xml";
+  exit();
+}
 
 $data = array('xmldata' => $_POST['xmldata']);
 
@@ -19,6 +34,7 @@ $options = array(
 $context  = stream_context_create($options);
 $result = file_get_contents($API_ENDPOINT, false, $context);
 
+echo "==========BEGIN IRIS RESPONSE==========\n";
 echo $result;
-
+echo "===========END IRIS RESPONSE===========\n";
 ?>
