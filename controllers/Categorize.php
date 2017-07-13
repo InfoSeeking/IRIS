93570006e94e6bbd3362e202509a6570a32bed7a
ChahnaP <chahnapatel2798@gmail.com>
2017-07-13 16:33:16 -0400

    Create Categorize.php

---
+++ controllers/Categorize.php
@@ -0,0 +1,10 @@
+<?php
+class Categorize extends Controller{
+           function run($xml){
+                  global $FILE_ROOT, $STORAGE, $REQ_ID, $CMD_EXTRA, $LIB, $BIN;
+                  
+                  $numCategories = intval($xml->numCategories);
+                  $arr = array();
+                  
+                  for($i=0;$i < numCategories; $i++){
+                  }
