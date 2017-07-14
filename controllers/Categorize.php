8ecb914e154d3703d8f2e322da8607ddaa884896
ChahnaP <chahnapatel2798@gmail.com>
2017-07-13 21:28:36 -0400

    Update Categorize.php

--- controllers/Categorize.php
+++ controllers/Categorize.php
@@ -7,4 +7,25 @@ class Categorize extends Controller{
                   $arr = array();
                   
                   for($i=0;$i < numCategories; $i++){
+                      $j = $i+1;
+                      $name = intval($xml->nameCat);
+                             if($i=0){
+                                $arr[0] = $name;
+                             }else{
+                                 $arr.add($name);
+                             }
+                             
                   }
+                  $i = 0;
+                  while($i < numCategories){
+                      $numDoc = intval($xml->numDoc);
+                           $k = 0;
+                           foreach($xml->resourceList2->resource2 as $res){
+                                      $arr[i][k] = $res;
+                                      $k++;
+                           }
+                                 
+                  }
+                  $i++;
+                      
+}                      
