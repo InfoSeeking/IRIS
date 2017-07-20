91219fd8d4dfbc26f33ef6eee59bb311bf13dc05
ChahnaP <chahnapatel2798@gmail.com>
2017-07-20 11:25:49 -0400

    Update return_files.php

--- controllers/return_files.php
+++ controllers/return_files.php
@@ -6,12 +6,13 @@ class returnFiles extends Controller{
           $nameCat = stringval($xml->nameCat);
           category::$arr;
           $arrlength = count($arr);
+          $newarray = array();
           
           for($i = 0; $i < $arrlength; $i++){
-            if($nameCat == $arr[i][0]){
-            $lengthcolumn = count($arr[i]);
+            if($nameCat == $arr[$i][0]){
+            $lengthcolumn = count($arr[$i]);
               for($j=0; $j < $lengthcolumn; $j++){
-                  echo $arr[i][j];
+                  $newarray[$i][$j] = $arr[$i][$j];
               }
             }else{
                   echo "Category not available";
@@ -22,13 +23,10 @@ class returnFiles extends Controller{
           
           }
       
+      
      
-
-
-
-
-
-
+      $response = "<parameters><requestType>filter</requestType>". $newarray. "</parameters>";
+      return $response;
 
 
 
