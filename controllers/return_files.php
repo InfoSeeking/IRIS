031bc0b92e4d7193ad3ce9cdfe9f71f0e14aabd6
ChahnaP <chahnapatel2798@gmail.com>
2017-07-20 09:59:28 -0400

    Update return_files.php

--- controllers/return_files.php
+++ controllers/return_files.php
@@ -4,12 +4,13 @@ class returnFiles extends Controller{
           global $FILE_ROOT, $STORAGE, $REQ_ID, $CMD_EXTRA, $LIB, $BIN;
           
           $nameCat = stringval($xml->nameCat);
+          category::$arr;
+          $arrlength = count($arr);
           
-          
-          foreach($i = 0; $i < $arrlength; $i++){
+          for($i = 0; $i < $arrlength; $i++){
             if($nameCat == $arr[i][0]){
             $lengthcolumn = count($arr[i]);
-              foreach($j=0; $j < $lengthcolumn; $j++){
+              for($j=0; $j < $lengthcolumn; $j++){
                   echo $arr[i][j];
               }
             }else{
