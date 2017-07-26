a3b978a248b41842380b314cd1f3a8e6ee79e339
ChahnaP <chahnapatel2798@gmail.com>
2017-07-26 14:05:55 -0400

    Update get_category.php

--- controllers/get_category.php
+++ controllers/get_category.php
@@ -1,5 +1,5 @@
 <?php
-    class getCategory extends Controller{
+    class GetCategory extends Controller{
              function run($xml){
              global $FILE_ROOT, $STORAGE, $REQ_ID, $CMD_EXTRA, $LIB, $BIN;
              
