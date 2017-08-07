78fe15ddd44bff76ec75c755bd60c8b252903e92
ChahnaP <chahnapatel2798@gmail.com>
2017-08-06 22:18:37 -0400

    Update get_category.php

--- controllers/get_category.php
+++ controllers/get_category.php
@@ -1,21 +1,21 @@
 <?php
     class get_category extends Controller{
              function run($xml){
-             global $FILE_ROOT, $STORAGE, $REQ_ID, $CMD_EXTRA, $LIB, $BIN, $arr;
+             global $FILE_ROOT, $STORAGE, $REQ_ID, $CMD_EXTRA, $LIB, $BIN;
              
              $file = $xml->filename;
              /*Categorize::$arr;*/
-             /*$Categorize = new Categorize();
-             echo $Categorize->arr;*/
-             $arrlength = count($arr);
+             $Categorize = new Categorize();
+             echo $Categorize->arr;
+             $arrlength = count(arr);
              $response = "<parameters>\n<requestID>" . $REQ_ID ."</requestID>\n<requestType>get_category</requestType>";
              
              for($i = 0; $i < $arrlength; $i++){
-             $lengthcolumn = count($arr[$i]);
+             $lengthcolumn = count(arr[$i]);
                 for($j = 0; $j < $lengthcolumn; $j++){
-                    if($arr[$i][$j] == $file){
-                    echo $arr[$i][$j];
-                    $response .= "<resource><id>" . $arr[$i][$j] . "</id>";
+                    if(arr[$i][$j] == $file){
+                    echo arr[$i][$j];
+                    $response .= "<resource><id>" . arr[$i][$j] . "</id>";
 		    $response .= "</resource>";
                     }
                 
