f4411357d7996ebff3bdb6406f53e56198a8998e
ChahnaP <chahnapatel2798@gmail.com>
2017-08-06 21:53:50 -0400

    Update get_category.php

--- controllers/get_category.php
+++ controllers/get_category.php
@@ -8,7 +8,7 @@
              /*$Categorize = new Categorize();
              echo $Categorize->arr;*/
              $arrlength = count($arr);
-             $response = "<parameters>\n<requestID>" . $REQ_ID ."</requestID>\n<requestType>getCategory</requestType>";
+             $response = "<parameters>\n<requestID>" . $REQ_ID ."</requestID>\n<requestType>get_category</requestType>";
              
              for($i = 0; $i < $arrlength; $i++){
              $lengthcolumn = count($arr[$i]);
