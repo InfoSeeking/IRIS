252642d8c5f2d948e2c8c4a7f23980f8bac9dc79
ChahnaP <chahnapatel2798@gmail.com>
2017-08-06 23:07:53 -0400

    Update categorize.php

--- controllers/categorize.php
+++ controllers/categorize.php
@@ -5,7 +5,7 @@ class Categorize extends Controller{
                   
                   
                   $numCategories = intval($xml->numCategories);
-                  public $arr = array();
+                  static $arr = array();
                   /*self::$arr = array();*/
                       
                   /*if(!pe($xml, "resourceList")) die(err("No resources found"));*/
