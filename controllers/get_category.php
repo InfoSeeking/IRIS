3e4254b194966c8ed7302bf20e2b7967e29680bb
ChahnaP <chahnapatel2798@gmail.com>
2017-08-08 13:19:36 -0400

    Update get_category.php

--- controllers/get_category.php
+++ controllers/get_category.php
@@ -4,18 +4,16 @@
              global $FILE_ROOT, $STORAGE, $REQ_ID, $CMD_EXTRA, $LIB, $BIN;
              
              $file = $xml->filename;
-             /*Categorize::$arr;*/
-             $Categorize = new Categorize();
-             echo $Categorize->arr;
-             $arrlength = count(arr);
+             Categorize::$arr;
+             $arrlength = count($arr);
              $response = "<parameters>\n<requestID>" . $REQ_ID ."</requestID>\n<requestType>get_category</requestType>";
              
              for($i = 0; $i < $arrlength; $i++){
-             $lengthcolumn = count(arr[$i]);
+             $lengthcolumn = count($arr[$i]);
                 for($j = 0; $j < $lengthcolumn; $j++){
-                    if(arr[$i][$j] == $file){
-                    echo arr[$i][$j];
-                    $response .= "<resource><id>" . arr[$i][$j] . "</id>";
+                    if($arr[$i][$j] == $file){
+                    echo $arr[$i][$j];
+                    $response .= "<resource><id>" . $arr[$i][$j] . "</id>";
 		    $response .= "</resource>";
                     }
                 
