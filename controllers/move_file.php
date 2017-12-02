c168800f9730a18394ff0884f5035d483b0456e6
Kelly Zhang <kelyz@users.noreply.github.com>
2017-12-02 14:07:24 -0500

    Added global CONTROLLER and fixed spacing

--- controllers/move_file.php
+++ controllers/move_file.php
@@ -1,25 +1,23 @@
 <?php
 
+global $CONTROLLER;
+require_once($CONTROLLER . "categorize.php");
 class MoveFile extends Controller{
     function run($xml){
     global $FILE_ROOT, $STORAGE, $REQ_ID, $CMD_EXTRA, $LIB, $BIN;
     
     $fileName = $xml->fileName;
     $nameCat = $xml->nameCat;
-    category::$arr;
+    categorize::$arr;
     $arrlength = count($arr);
     
       for($i = 0; $i < $arrlength; $i++){
         if($arr[$i][0] == $nameCat){
-        $lengthcolumn = count($arr[$i]);
-          
+            $lengthcolumn = count($arr[$i]);
             $arr[$i][$lengthcolumn] = $fileName;
-          
-        
         }
-      
       }
+      $response = "<parameters><requestType>move_file</requestType>". "Done!";
+      return $response;
     }
-    $response = "<parameters><requestType>move_file</requestType>". "Done!";
-    return $response;
 }    
